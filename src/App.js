533000b82027f0b7232c29232b8ad669ed858835
santhoshkumar <santhoshofficial32@gmail.com>
2022-07-23 21:00:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>{
+   const inputArea=document.querySelector(".workspace-input");
+   const outputArea=document.querySelector(".workspace-output");
+   const btnFormat=document.querySelector(".control-format");
+   const btnClear=document.querySelector(".control-clear");
+   
+   btnFormat.addEventListener("click",(e)=>{
+      e.preventDefault();
+      try{
+           const formatted=JSON.stringify(JSON.parse(inputArea.value),null,4);
+            outputArea.value=formatted;
+      }
+      catch(err)
+      {
+        outputArea.value=err.message;
+      }
+    });
+    btnClear.addEventListener("click",()=>{
+      inputArea.value="";
+      outputArea.value="";
+    })
+    return (
+      <div className="container">
+         <div className="buttonControls">
+         <button className="controls control-format">Format </button>
+         <button className="controls control-clear" >Clear</button>
+        </div>
+        <div className="screenArea">
+          <textarea className="workspace workspace-input" spellCheck={false} placeholder="Enter Your Json here">
+        </textarea>
+        <textarea  readOnly={true} className="workspace workspace-output"  id="outputArea" placeholder="Formated json will appear here">
+        </textarea>
+        </div>
+      </div>
+    );
 }
 
 export default App;
